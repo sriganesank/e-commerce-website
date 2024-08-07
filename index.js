808787c8cc1c3eee3d3f2f32a61dfdc18ddc1912
sriganesank <sri715565@gmail.com>
2024-08-07 10:42:14 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+
+    var ProductImg = document.getElementById(productImg);
+     var SmallImg = document.getElementsByClassName(small-img);
+
+     smallimg[0].onclick = function(){
+        ProductImg.src = SmallImg[0].src;
+     }
+     smallimg[1].onclick = function(){
+        ProductImg.src = SmallImg[1].src;
+     }
+     smallimg[2].onclick = function(){
+        ProductImg.src = SmallImg[2].src;
+     }
+     smallimg[3].onclick = function(){
+        ProductImg.src = SmallImg[3].src;
+     }
